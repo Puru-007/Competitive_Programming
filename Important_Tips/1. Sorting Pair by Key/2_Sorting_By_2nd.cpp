f8ead60cc9d936e62fee026075a43adf2fb986f4
Puru-007 <purveshpatil660@gmail.com>
2022-01-18 16:51:51 +0530

    Sorting Pair tips added

---
+++ Important_Tips/1. Sorting Pair by Key/2_Sorting_By_2nd.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+
+bool sortBySec (pair <int, int> a, pair <int, int> b) {
+    return (a.second<b.second);
+}
+
+
+int main() {
+    int arr[] = {5, 3, 6, 1, 2, 4};
+    int arr1[] = {100, 500, 300, 200, 600, 400};
+
+    vector<pair<int, int>> vec;
+    for(int i=0; i<6; i++) {
+        // vec.pb(make_pair(arr[i], arr1[i]));
+        vec.pb(mp(arr[i], arr1[i]));
+    }
+
+    sort(vec.begin(), vec.end(), sortBySec);
+    
+    for(int i=0; i<6; i++) {
+        cout<<vec[i].first<<"->"<<vec[i].second<<endl;
+    }
+}
