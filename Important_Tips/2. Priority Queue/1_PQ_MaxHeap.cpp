47b23382f9e1735cfb6ada879db838f359b1b5d5
Puru-007 <purveshpatil660@gmail.com>
2022-01-29 13:03:28 +0530

    Priority min and max heap syntax imp

---
+++ Important_Tips/2. Priority Queue/1_PQ_MaxHeap.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool sortMax(int num1, int num2){
+    return num1 > num2;
+}
+
+int main() {
+	// your code goes here
+	int N;
+	cin>>N;
+	priority_queue<int, vector<int>> pq;
+	for(int i=0; i<N; i++) {
+	    int num;
+	    cin>>num;
+	    pq.push(num);
+	}
+	
+	while(pq.size() != 0) {
+	    cout<<pq.top()<<" ";
+	    pq.pop();
+	}
+	return 0;
+}
